e9f80dcd009cb782c67442dea363a20fe9bed21b
Ahmed Yasser <ahmedreo4@gmail.com>
2023-08-25 09:51:07 +0300

    html

---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="navbar">
+    <div class="container">
+      <div class="header">
+        <div class="logo"></div>
+        <div class="actions">
+          <button><img src="@/assets/img/user.svg" alt="user">تسجيل الدخول</button>
+          <button><img src="@/assets/img/plus.svg" alt="user">انضم الينا</button>
+        </div>
+      </div>
+      <ul class="nav-links">
+        <li><a href="#">الرئيسية</a></li>
+        <li><a href="#">الخدمات</a></li>
+        <li><a href="#">الصالونات</a></li>
+        <li><a href="#">كيف تستخدم هيرموسا</a></li>
+        <li><a href="#">حمل التطبيق</a></li>
+        <li><a href="#">اتصل بنا</a></li>
+      </ul>
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,94 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Navbar></Navbar>
+    <div id="hero">
+      <div class="card">
+        <div class="title">هيرموسا</div>
+        <div class="description">
+          برنامج محاسبي متكامل مخصص للصالونات النسائية معتمد من قبل هيئة الزكاة والدخل
+        </div>
+        <button>أقراء المزيد</button>
+      </div>
+    </div>
+    <div id="our-services">
+      <div class="title">خدماتنا</div>
+      <ul>
+        <li><img src="@/assets/img/makeup.svg" alt="makeup"><a href="#">المكياج</a></li>
+        <li><img src="@/assets/img/hair-cut.svg" alt="hair-cut"><a href="#">جهاز شير لإزالة الشعر</a></li>
+        <li><img src="@/assets/img/spa.svg" alt="spa"><a href="#">سبا</a></li>
+        <li><img src="@/assets/img/skin.svg" alt="skin"><a href="#">تنظيف البشرة</a></li>
+        <li><img src="@/assets/img/eyes.svg" alt="eyes"><a href="#">رموش</a></li>
+      </ul>
+    </div>
+    <div id="saloons">
+      <div class="title">صالونتنا</div>
+      <div class="slider">
+        <div class="slider-controls">
+          <a href="#"><img src="@/assets/img/arrow-right.svg" alt="arrow"></a>
+          <a href="#"><img src="@/assets/img/arrow-left.svg" alt="arrow"></a>
+        </div>
+        <div class="slider-items">
+          <div class="slide" v-for="i in [1, 2, 3]">
+            <div class="card">
+              <div class="gallery"><img src="@/assets/img/saloon.png" alt="salon"></div>
+              <div class="title">اسم الصالون</div>
+              <div class="description">المدينة</div>
+              <div class="btn btn-primary">المزيد</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div id="how-to-use">
+      <div class="title">كيف تستخدم هيرموسا</div>
+      <div class="steps">
+        <div class="step"><img src="@/assets/img/mobile-1.png"></div>
+        <div class="step"><img src="@/assets/img/mobile-2.png"></div>
+        <div class="step"><img src="@/assets/img/mobile-3.png"></div>
+      </div>
+    </div>
+    <div id="download-app">
+      <div class="app-img"><img src="@/assets/img/app.png" alt="app"></div>
+      <div class="get-btns">
+        <div class="title"><span>قم بتحميل تطبيقنا من خلال</span></div>
+        <div>
+          <a href="#" class="btn"><img src="@/assets/img/google-play.png" alt="google-play"></a>
+          <a href="#" class="btn"><img src="@/assets/img/app-store.png" alt="app-store"></a>
+        </div>
+      </div>
+    </div>
+    <div id="contact-us">
+      <div class="section-img"><img src="@/assets/img/contact-us.png" alt="contact-us"></div>
+      <form action="#">
+        <div class="input-group">
+          <div class="label">الاسم</div>
+          <input type="text" placeholder="الاسم" />
+        </div>
+        <div class="input-group">
+          <div class="label">البريد الالكتروني</div>
+          <input type="text" placeholder="البريد الالكتروني" />
+        </div>
+        <div class="input-group">
+          <div class="label">الرسالة</div>
+          <textarea type="text" placeholder="الرسالة"></textarea>
+        </div>
+        <div class="input-group">
+          <button type="submit" class="btn btn-primary">ارسال</button>
+        </div>
+      </form>
+    </div>
+    <div id="footer">
+
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+<script>
+import Navbar from "@/components/Navbar.vue";
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  components: { Navbar }
 }
 </script>
+
+<style lang="scss"></style>
